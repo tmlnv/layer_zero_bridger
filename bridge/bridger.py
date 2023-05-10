30f5ac76807709e3cdc709d26827f33375509eea
Artem Leonov <tmlnv@protonmail.com>
2023-05-11 01:31:08 +0300

    Fixed problem with incorrect amout bridging when balance is lower that desired amount.

--- bridge/bridger.py
+++ bridge/bridger.py
@@ -6,6 +6,7 @@ from loguru import logger
 
 from web3 import AsyncWeb3
 from web3.contract import AsyncContract
+from web3.exceptions import ValidationError
 
 from config import AMOUNT_TO_SWAP
 from utils.params import usdc_addresses
@@ -99,31 +100,35 @@ async def send_usdc_chain_to_chain(
 
     elif usdc_balance < AMOUNT_TO_SWAP:
 
-        min_amount = usdc_balance - usdc_balance * 0.005
+        try:
+            min_amount = round(usdc_balance * 0.99)
 
-        swap_txn = await stargate_from_chain_contract.functions.swap(
-            transaction_info["chain_id"],
-            transaction_info["source_pool_id"],
-            transaction_info["dest_pool_id"],
-            transaction_info["refund_address"],
-            transaction_info["amount_in"],
-            min_amount,
-            transaction_info["lz_tx_obj"],
-            transaction_info["to"],
-            transaction_info["data"]
-        ).build_transaction(
-            {
-                'from': address,
-                'value': fee,
-                'gas': 500000,
-                'gasPrice': await from_chain_w3.eth.gas_price,
-                'nonce': await from_chain_w3.eth.get_transaction_count(address),
-            }
-        )
+            swap_txn = await stargate_from_chain_contract.functions.swap(
+                transaction_info["chain_id"],
+                transaction_info["source_pool_id"],
+                transaction_info["dest_pool_id"],
+                transaction_info["refund_address"],
+                usdc_balance,
+                min_amount,
+                transaction_info["lz_tx_obj"],
+                transaction_info["to"],
+                transaction_info["data"]
+            ).build_transaction(
+                {
+                    'from': address,
+                    'value': fee,
+                    'gas': 500000,
+                    'gasPrice': await from_chain_w3.eth.gas_price,
+                    'nonce': await from_chain_w3.eth.get_transaction_count(address),
+                }
+            )
 
-        signed_swap_txn = from_chain_w3.eth.account.sign_transaction(swap_txn, wallet)
-        swap_txn_hash = await from_chain_w3.eth.send_raw_transaction(signed_swap_txn.rawTransaction)
-        return swap_txn_hash
+            signed_swap_txn = from_chain_w3.eth.account.sign_transaction(swap_txn, wallet)
+            swap_txn_hash = await from_chain_w3.eth.send_raw_transaction(signed_swap_txn.rawTransaction)
+            return swap_txn_hash
+
+        except ValidationError as e:
+            logger.error(f'Amount to be bridged is too low. Attempting raised an {e}')
 
 
 async def check_balance(address: str, usdc_contract: AsyncContract) -> int:
