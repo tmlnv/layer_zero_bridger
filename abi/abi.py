f34b5b0318c04a7a3c284b5f40b2fc9b5eaa9ca6
Yuriy Kachurovskiy <yokachurovskiy@sbercloud.ru>
2023-07-07 14:17:56 +0300

    Refactoring

--- abi/abi.py
+++ abi/abi.py
@@ -2,9 +2,9 @@
 import json
 import os
 
-with open(os.path.join(os.path.abspath(os.path.join(__file__, os.path.pardir)), 'router_abi.json')) as file:
-    stargate_abi = json.load(file)
-with open(os.path.join(os.path.abspath(os.path.join(__file__, os.path.pardir)), 'usdc_abi.json')) as file:
-    usdc_abi = json.load(file)
-with open(os.path.join(os.path.abspath(os.path.join(__file__, os.path.pardir)), 'usdt_abi.json')) as file:
-    usdt_abi = json.load(file)
+with open(os.path.join(os.path.abspath(os.path.join(__file__, os.path.pardir)), 'router_abi.json')) as router, \
+        open(os.path.join(os.path.abspath(os.path.join(__file__, os.path.pardir)), 'usdc_abi.json')) as usdc, \
+        open(os.path.join(os.path.abspath(os.path.join(__file__, os.path.pardir)), 'usdt_abi.json')) as usdt:
+    stargate_abi = json.load(router)
+    usdc_abi = json.load(router)
+    usdt_abi = json.load(usdt)
